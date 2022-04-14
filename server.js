88414e64e1b708efa309908124c3897c2c0071a9
Kallum Tolkien <kallumtolkien@gmail.com>
2022-04-14 15:21:01 +0100

    testing signin and adding json functionality

--- server.js
+++ server.js
@@ -4,13 +4,45 @@ const express = require('express');
 // create our express app
 const app = express();
 
+// we use this below instead of body parser and will need to add this below our express app in most cases
+app.use(express.json());
+
+// Create a mock database because we haven't learned how to create a proper one yet
+const database = {
+  users: [
+    {
+      id: '123',
+      name: 'John',
+      email: 'john@gmail.com',
+      password: 'cookies',
+      entries: 0, // number of times the user has uploaded a picture
+      joined: new Date(), // creates a new date when the user is created - a built-in js function
+    },
+    {
+      id: '124',
+      name: 'Sally',
+      email: 'sally@gmail.com',
+      password: 'bananas',
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+}; // our database is an object containing the value users which is an array of objects containing all our user data
+
 app.get('/', (req, res) => {
   res.send('this is working');
 }); // use postman to send a GET request to localhost:3000/ and we will receive 'this is working' as the response
 
 app.post('/signin', (req, res) => {
-  res.send('signing in');
-});
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json('success'); // checking the request email and password information matches our database
+  } else {
+    res.status(400).json('error logging in'); // else send an error message and set the status to 400
+  }
+}); // POST request to localhost:3000/signin to receive our response
 
 // add a port for our app to listen to (3000), with a console log message to show it is working (even though nodemon kind of does this)
 app.listen(3000, () => {
