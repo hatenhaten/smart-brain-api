c7afa92f01aedb65d6edaf82aa7e1b4eb447c3dc
Kallum Tolkien <kallumtolkien@gmail.com>
2022-04-14 14:58:36 +0100

    signin route set up

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.send('this is working');
-}) // use postman to send a GET request to localhost:3000/ and we will receive 'this is working' as the response
+}); // use postman to send a GET request to localhost:3000/ and we will receive 'this is working' as the response
+
+app.post('/signin', (req, res) => {
+  res.send('signing in');
+});
 
 // add a port for our app to listen to (3000), with a console log message to show it is working (even though nodemon kind of does this)
 app.listen(3000, () => {
@@ -15,11 +19,8 @@ app.listen(3000, () => {
 
 // Now to  think about what our App will look like in terms of routes / endpoints. This may change throughout the project but it is worth doing as a start point:
 
-/*
-/ --> res = this is working
-/signin --> POST (user information) = success/fail
-/register --> POST (user information) = user (object that we will return)
-/profile/:userId --> GET = user
-/image --> PUT = user (updates the user information of number of images posted + the rank they receive compared to other users)
-
-*/
+// / --> res = this is working
+// - signin --> POST (user information) = success/fail
+// /register --> POST (user information) = user (object that we will return)
+// /profile/:userId --> GET = user
+// /image --> PUT = user (updates the user information of number of images posted + the rank they receive compared to other users)
